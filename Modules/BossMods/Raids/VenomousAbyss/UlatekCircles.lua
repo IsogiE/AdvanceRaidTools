e4c2400dbc53d99f83b1e88a8ee22fe8eda78512
IsogiE <88090493+IsogiE@users.noreply.github.com>
2026-09-12 19:21:02 +0200

    Fix Ula'tek aura alerts and Purge line cleanup (#18)

--- Modules/BossMods/Raids/VenomousAbyss/UlatekCircles.lua
+++ Modules/BossMods/Raids/VenomousAbyss/UlatekCircles.lua
@@ -28,9 +28,7 @@ local DEFINITIONS = {
         timerSpellID = SERPENTS_BITE_TIMER_ID,
         maxDuration = 15,
         windowDuration = 15,
-        audio = {
-            ttsText = "Serpent's Bite"
-        },
+        audio = {},
         color = {1, 0.08, 0.08, 0.95},
         position = {point = "CENTER", x = 0, y = 0}
     },
